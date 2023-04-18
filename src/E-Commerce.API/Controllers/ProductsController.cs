23103dfdf3ef57ac50e7884ff11c84e72f0ff357
DMPatod <danielmaiaevangelista@gmail.com>
2023-04-18 19:07:13 -0300

    initial commit

---
+++ src/E-Commerce.API/Controllers/ProductsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+    }
+}
